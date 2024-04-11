7966b5355ce23462af1adfbf09542496a9671714
riffsphereha <riffsphereha@gmail.com>
2024-04-11 09:31:20 -0700

    update startup.sh and dockerfile
    Put a default config file in the docker image, and copy it to the /config folder on startup if no config file is detected

--- downloadarr/startup.sh
+++ downloadarr/startup.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Create config file if not exits
+CONFIG_FILE="/config/config.yml"
+EXAMPLE_CONFIG_FILE="/app/config/example_config.yml"
+
+# Check if the config file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+    # If the config file doesn't exist, copy it from the example config
+    cp "$EXAMPLE_CONFIG_FILE" "$CONFIG_FILE"
+    echo "Config file copied successfully."
+else
+    echo "Config file already exists."
+fi
+
 # Set the cron schedule from the CRON_SCHEDULE environment variable
 CRON_SCHEDULE=${CRON_SCHEDULE:-"0 * * * *"}
 
